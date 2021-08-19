6e586288236ab0c8b6ee725c99e73c44dccf1ae3
Cayla Cardiff <cayla.cardiff@gmail.com>
2021-08-19 11:23:39 -0500

    Added second option with notes for bubbleSort.

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,8 +1,9 @@
-// bubbleSort is long and slow with randomized things;
-// it's going to iterate through from i to the end,
-//   and within that, iterate from i to the end again (so O(n^2))
-//   each time swapping a pair for low/high if needed.
-// this happens in place, so it's not bad on space usage.
+// BubbleSort is long and slow - it's best and worst case is O(n^2).
+// Like combing out tangles one by one with a fine-tooth comb.
+// Going to iterate through from beginning to end repeatedly,
+//   and within that, swap the current item with the next item, if needed;
+//   the element at hand will "bubble" up to its rightful place.
+// Space complexity is at least good: no recursion and you change the existing array in place so: O(1).
 
 bubbleSort = (array) => {
   // set up your swapped variable:
@@ -11,6 +12,7 @@ bubbleSort = (array) => {
   do {
     swapped = false;
     // first 2 args to forEach are element and index:
+    // note that current and index are going to track together.
     array.forEach((current, i) => {
       // so this is current and the one after it:
       if (current > array[i + 1]) {
@@ -21,10 +23,29 @@ bubbleSort = (array) => {
         swapped = true;
       }
       // swapped is true, so the iteration will continue - and the first thing that happens is it resets to false.
-      // after the last iteration, it will remain at "true" and then we know we're at the end.
+      // the algorithm needs one whole pass without a swap to know that it's done.
     })
     // the "while" at the end is the backstop; here while "swapped" is true:
   } while (swapped);
   // you're done!
+  return array;
+}
+
+// BUT WAIT! THERE'S ANOTHER WAY:
+// You can do this with nested loops and, once an element has "bubbled" to its rightful place, you can ignore the rest of the array after that.
+// The inner loop has to get smaller every iteration it makes to do this.
+// This aligns more with the videos you'll see, like the dancers - when they're in place, they turn around and stand still.
+bubbleSort = (array) => {
+  let swapped;
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
   return array;
 }
